6c30509d8a032dce833145fec0e3d00f438bb333
divyanshdwivedi08 <dwivedidivyansh24@gmail.com>
2026-05-19 00:04:12 +0530

    New Changes

--- demo/src/main/java/com/hotel/management/demo/Controller/HotelController.java
+++ demo/src/main/java/com/hotel/management/demo/Controller/HotelController.java
@@ -1,12 +1,13 @@
 package com.hotel.management.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HotelController {
-@GetMapping
-public String returnSomethingMan() {
-	return "Hey Bro";
+@GetMapping("/heyBro")
+public String returnSomethingMan(@RequestParam String name) {
+	return "Hey Bro"+ " "+name;
 }
 }
